21ec308fa4f6f4abcd8dc07586e73e2fbb1c3faa
AleksandrYahno <aleksandryahno@gmail.com>
2019-02-12 22:36:53 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+console.log('check');
+
+$(document).ready(function ($) {
+
+    $(function () {
+        $("#tabs").tabs();
+    });
+
+    $("#topNavigation li a").click(function () {
+        var e = $(this).parent().parent().find("a");
+        for (var i = 0; i < e.length; i++) {
+            e[i].setAttribute("class", " ");
+        }
+        $(this).addClass("active-navigation");
+    });
+
+    $("#bottomNavigation li a").click(function () {
+        var e = $(this).parent().parent().find("a");
+        for (var i = 0; i < e.length; i++) {
+            e[i].setAttribute("class", " ");
+        }
+        $(this).addClass("active-navigation");
+    });
+
+
+    $( ".accordion" ).accordion({
+        active: false,
+        collapsible: true
+    });
+
+});
+
